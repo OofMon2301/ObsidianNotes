7ce5fb3968935d62786a16fc194b88272fb96166
Jamie Chen <jamiechen2301@gmail.com>
2024-03-18 12:54:08 +1000

    Quartz sync: Mar 18, 2024, 12:54 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -60,7 +60,7 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
-          dark: "github-dark",
+          dark: "catppuccin-mocha",
         },
         keepBackground: false,
       }),
